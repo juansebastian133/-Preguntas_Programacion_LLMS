3898b55d00ecf2f5ade01ba9898b03c253683f8e
juansebastian133 <138958370+juansebastian133@users.noreply.github.com>
2026-05-30 16:20:21 -0500

    Update answer-434.py

--- myanswers/answer-434.py
+++ myanswers/answer-434.py
@@ -1,29 +1,22 @@
-import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.ensemble import RandomForestClassifier
 
 def pipeline_seleccion_modelo(X, y, k):
-    # Escalar variables
+
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
 
-    # Seleccionar las k mejores variables
     selector = SelectKBest(score_func=f_classif, k=k)
     X_selected = selector.fit_transform(X_scaled, y)
 
-    # Obtener nombres de las variables seleccionadas
     selected_indices = selector.get_support(indices=True)
     selected_features = [X.columns[i] for i in selected_indices]
 
-    # Entrenar modelo
     model = RandomForestClassifier()
     model.fit(X_selected, y)
 
-    # Importancias
-    importancias = model.feature_importances_
-
     return {
         "variables_seleccionadas": selected_features,
-        "importancias": importancias
+        "importancias": model.feature_importances_
     }
