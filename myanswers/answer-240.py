81b3de05973b2ab20d4f0a3a2f8d29c76d5add1d
juansebastian133 <138958370+juansebastian133@users.noreply.github.com>
2026-05-30 16:09:28 -0500

    Update answer-240.py

--- myanswers/answer-240.py
+++ myanswers/answer-240.py
@@ -1,1 +1,12 @@
+from sklearn.inspection import permutation_importance
 
+def calcular_importancia_permutacion(modelo, X_val, y_val):
+    resultado = permutation_importance(
+        modelo,
+        X_val,
+        y_val,
+        n_repeats=5,
+        random_state=42
+    )
+
+    return resultado.importances_mean
